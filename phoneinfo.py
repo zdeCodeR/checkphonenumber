2986e213ef6cebce667d4b4c7ee23409409ca28f
zdeCodeR <arabs3866@gmail.com>
2025-08-30 11:23:30 +0700

    Create phoneinfo.py

---
+++ phoneinfo.py
@@ -0,0 +1,58 @@
+import sys
+import requests
+import json
+import time
+
+def banner():
+	print('''
+	Author: azamTukam
+	Github: https://github.com/zdeCodeR            
+	''')
+
+def main():
+	banner()
+	number = input('\33[m[\033[93m•\33[m] MASUKKAN NOMER TELEPON ( 62 ): \x1b[1;92m')
+	output = requests.get(f'http://phone-number-api.com/json/?number={number}').text
+	obj = json.loads(output)
+		
+	query = obj['query']
+	status = obj['status']
+	numberType = obj['numberType']
+	numberCountryCode = obj['numberCountryCode']
+	numberAreaCode = obj['numberAreaCode']
+	ext = obj['ext']
+	dialFromCountryCode = obj['dialFromCountryCode']
+	carrier = obj['carrier']
+	continent = obj['continent']
+	continentCode = obj['continentCode']
+	country = obj['country']
+	countryName = obj['countryName']
+	lat = obj['lat']
+	lon = obj['lon']
+	timezone = obj['timezone']
+	offset = obj['offset']
+	currency = obj['currency']
+		
+	print('[+] Information Output')
+	print('--------------------------------------')
+	print(' - Phone number:', query)
+	print(' - Status:', status)
+	print(' - Number type:', numberType)
+	print(' - Number Country Code:', numberCountryCode)
+	print(' - Number Area Code:', numberAreaCode)
+	print(' - ext:', ext)
+	print(' - dial country:', dialFromCountryCode)
+	print(' - carrier:', carrier)
+	print(' - continent:', continent)
+	print(' - continent code:', continentCode)
+	print(' - country:', country)
+	print(' - countryname:', countryName)
+	print(' - Latitude:', lat)
+	print(' - Longitude:', lon)
+	print(' - timezone:', timezone)
+	print(' - offset:', offset)
+	print(' - currency:', currency)
+
+if __name__ == '__main__':
+	main()
+  
